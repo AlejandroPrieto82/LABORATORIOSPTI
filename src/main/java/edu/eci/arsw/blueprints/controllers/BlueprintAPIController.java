a36b7883611dc6628b8f20ea327f9fcc7ed4e97c
Alejandro PR <samuel.prieto-r@mail.escuelaing.edu.co>
2025-03-25 17:55:54 -0500

    Cambios finales

--- src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
+++ src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.eci.arsw.blueprints.model.Blueprint;
@@ -27,6 +26,7 @@ public class BlueprintAPIController {
 
   @Autowired
   private BlueprintsServices blueprintsServices;
+
   @GetMapping()
   public ResponseEntity<Set<Blueprint>> getAllBlueprints() {
       try {
@@ -38,48 +38,42 @@ public class BlueprintAPIController {
       }
   }
   
-
   @GetMapping("/{author}")
-public ResponseEntity<Set<Blueprint>> getBlueprintsByAuthor(@PathVariable String author) {
-    try {
-        Set<Blueprint> blueprints = blueprintsServices.getBlueprintsByAuthor(author);
-        if (blueprints.isEmpty()) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND); // Retorna 404 si no hay planos
-        }
-        return new ResponseEntity<>(blueprints, HttpStatus.OK);
-    } catch (Exception e) {
-        Logger.getLogger(BlueprintAPIController.class.getName()).log(Level.SEVERE, null, e);
-        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-}
+  public ResponseEntity<Set<Blueprint>> getBlueprintsByAuthor(@PathVariable String author) {
+      try {
+          Set<Blueprint> blueprints = blueprintsServices.getBlueprintsByAuthor(author);
+          if (blueprints.isEmpty()) {
+              return new ResponseEntity<>(HttpStatus.NOT_FOUND); // Retorna 404 si no hay planos
+          }
+          return new ResponseEntity<>(blueprints, HttpStatus.OK);
+      } catch (Exception e) {
+          Logger.getLogger(BlueprintAPIController.class.getName()).log(Level.SEVERE, null, e);
+          return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+      }
+  }
 
-@GetMapping("/{author}/{bpname}")
-public ResponseEntity<Blueprint> getBlueprint(
-    @PathVariable String author,
-    @PathVariable String bpname
-) {
-    try {
-        Blueprint blueprint = blueprintsServices.getBlueprint(author, bpname);
-        return new ResponseEntity<>(blueprint, HttpStatus.OK);
-    } catch (Exception e) {
-        Logger.getLogger(BlueprintAPIController.class.getName()).log(Level.SEVERE, null, e);
-        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-}
+  @GetMapping("/{author}/{bpname}")
+  public ResponseEntity<Blueprint> getBlueprint(
+      @PathVariable String author,
+      @PathVariable String bpname
+  ) {
+      try {
+          Blueprint blueprint = blueprintsServices.getBlueprint(author, bpname);
+          return new ResponseEntity<>(blueprint, HttpStatus.OK);
+      } catch (Exception e) {
+          Logger.getLogger(BlueprintAPIController.class.getName()).log(Level.SEVERE, null, e);
+          return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+      }
+  }
 
   @PostMapping()
-  public ResponseEntity<?> addNewBlueprint(@RequestBody Blueprint blueprint) {
-    try {
-      blueprintsServices.addNewBlueprint(blueprint);
-      return new ResponseEntity<>(HttpStatus.CREATED);
-    } catch (Exception e) {
-      Logger
-        .getLogger(BlueprintAPIController.class.getName())
-        .log(Level.SEVERE, null, e);
-      return new ResponseEntity<>(
-        "Error al agregar el blueprint",
-        HttpStatus.INTERNAL_SERVER_ERROR
-      );
-    }
+  public ResponseEntity<String> addNewBlueprint(@RequestBody Blueprint blueprint) { // Cambio en el tipo de retorno
+      try {
+          blueprintsServices.addNewBlueprint(blueprint);
+          return new ResponseEntity<>("Blueprint agregado correctamente", HttpStatus.CREATED);
+      } catch (Exception e) {
+          Logger.getLogger(BlueprintAPIController.class.getName()).log(Level.SEVERE, null, e);
+          return new ResponseEntity<>("Error al agregar el blueprint", HttpStatus.INTERNAL_SERVER_ERROR);
+      }
   }
 }
